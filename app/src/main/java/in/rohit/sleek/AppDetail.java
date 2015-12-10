784d840b1585918861a7b805600c53f471051f91
rohittanwar <rohit_annpurna@rediff.com>
2015-12-10 16:28:49 +0530

    'basic activity changes'

---
+++ app/src/main/java/in/rohit/sleek/AppDetail.java
@@ -0,0 +1,12 @@
+package in.rohit.sleek;
+
+/**
+ * Created by Student on 10-12-2015.
+ */
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
